1a976d68206fa3eb4cf4c867d12ce09f85e2ea4a
FORGE-JITA\ktokt <ulfnew@gmail.com>
2019-01-19 01:37:00 +0500

    Update database setup scripts

--- src/Environment/Storage/Controller.php
+++ src/Environment/Storage/Controller.php
@@ -15,140 +15,147 @@ class Controller extends \Environment\Controller
     const INSTALL = '
 CREATE TABLE IF NOT EXISTS occupancy_type
 (
-  id   INTEGER
-    primary key
-  autoincrement,
-  code TEXT
-);
+    id INTEGER
+        primary key
+         autoincrement,
+    code TEXT
+)
+;
 
-create unique index IF NOT EXISTS occupancy_type_code_uindex
-  on occupancy_type (code);
-
-CREATE TABLE IF NOT EXISTS role
-(
-  id   INTEGER
-    primary key
-  autoincrement,
-  code TEXT not null
-);
-
-create unique index IF NOT EXISTS role_code_uindex
-  on role (code);
+CREATE UNIQUE INDEX IF NOT EXISTS occupancy_type_code_uindex
+    on occupancy_type (code)
+;
 
 CREATE TABLE IF NOT EXISTS shutter
 (
-  id     INTEGER
-    primary key
-  autoincrement,
-  remark TEXT,
-  point  TEXT
-);
+    id INTEGER
+        primary key
+         autoincrement,
+    remark TEXT,
+    point TEXT
+)
+;
 
-create index IF NOT EXISTS shutter_point_id_index
-  on shutter (point, id);
+CREATE TABLE IF NOT EXISTS renting
+(
+    id INTEGER
+        primary key
+         autoincrement,
+    shutter_id INTEGER
+        constraint renting_shutter_id_fk
+            references shutter
+)
+;
 
-create unique index IF NOT EXISTS shutter_point_uindex
-  on shutter (point);
+CREATE UNIQUE INDEX IF NOT EXISTS renting_shutter_id_uindex
+    on renting (shutter_id)
+;
+
+CREATE INDEX IF NOT EXISTS shutter_point_id_index
+    on shutter (point, id)
+;
+
+CREATE UNIQUE INDEX IF NOT EXISTS shutter_point_uindex
+    on shutter (point)
+;
 
 CREATE TABLE IF NOT EXISTS unlock
 (
-  shutter_id INTEGER
-    constraint unlock_shutter_id_fk
-    references shutter
-);
+    shutter_id INTEGER
+        constraint unlock_shutter_id_fk
+            references shutter
+)
+;
 
-create unique index IF NOT EXISTS unlock_shutter_id_uindex
-  on unlock (shutter_id);
+CREATE UNIQUE INDEX IF NOT EXISTS unlock_shutter_id_uindex
+    on unlock (shutter_id)
+;
 
 CREATE TABLE IF NOT EXISTS "user"
 (
-  id     INTEGER
-    primary key
-  autoincrement,
-  email  TEXT,
-  secret TEXT
-);
-
-create index IF NOT EXISTS user_email_secret_index
-  on "user" (email, secret);
-
-create unique index IF NOT EXISTS user_email_uindex
-  on "user" (email);
+    id INTEGER
+        primary key
+         autoincrement,
+    email TEXT,
+    secret TEXT
+)
+;
 
 CREATE TABLE IF NOT EXISTS lease
 (
-  id                INTEGER
-    primary key
-  autoincrement,
-  user_id           INTEGER
-    constraint occupancy_user_id_fk
-    references "user",
-  shutter_id        INTEGER
-    constraint occupancy_shutter_id_fk
-    references shutter,
-  start             INTEGER,
-  finish            INTEGER,
-  occupancy_type_id INTEGER
-    constraint occupancy_occupancy_type_id_fk
-    references occupancy_type
-);
+    id INTEGER
+        primary key
+         autoincrement,
+    user_id INTEGER
+        constraint occupancy_user_id_fk
+            references "user",
+    shutter_id INTEGER
+        constraint occupancy_shutter_id_fk
+            references shutter,
+    start INTEGER,
+    finish INTEGER,
+    occupancy_type_id INTEGER
+        constraint occupancy_occupancy_type_id_fk
+            references occupancy_type
+)
+;
 
-create index IF NOT EXISTS occupancy_user_id_start_finish_shutter_id_index
-  on lease (user_id, start, finish, shutter_id);
+CREATE INDEX IF NOT EXISTS occupancy_user_id_start_finish_shutter_id_index
+    on lease (user_id, start, finish, shutter_id)
+;
 
 CREATE TABLE IF NOT EXISTS session
 (
-  id      INTEGER
-    primary key
-  autoincrement,
-  token   TEXT,
-  finish  INTEGER,
-  user_id INTEGER
-    constraint session_user_id_fk
-    references "user"
-);
+    id INTEGER
+        primary key
+         autoincrement,
+    token TEXT,
+    finish INTEGER,
+    user_id INTEGER
+        constraint session_user_id_fk
+            references "user"
+)
+;
 
-create index IF NOT EXISTS session_finish_token_user_id_index
-  on session (finish, token, user_id);
+CREATE INDEX IF NOT EXISTS session_finish_token_user_id_index
+    on session (finish, token, user_id)
+;
 
-CREATE TABLE IF NOT EXISTS user_role
-(
-  id      INTEGER
-    primary key
-  autoincrement,
-  user_id INTEGER
-    constraint user_role_user_id_fk
-    references "user",
-  role_id INTEGER
-    constraint user_role_role_id_fk
-    references role
-);
+CREATE INDEX IF NOT EXISTS user_email_secret_index
+    on "user" (email, secret)
+;
 
-create unique index IF NOT EXISTS user_role_user_id_role_id_uindex
-  on user_role (user_id, role_id);
-  
-CREATE TABLE IF NOT EXISTS renting
-(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    shutter_id INTEGER,
-    CONSTRAINT renting_shutter_id_fk FOREIGN KEY (shutter_id) REFERENCES shutter (id)
-);
-CREATE UNIQUE INDEX IF NOT EXISTS renting_shutter_id_uindex ON renting (shutter_id);
+CREATE UNIQUE INDEX IF NOT EXISTS user_email_uindex
+    on "user" (email)
+;
+'.'
+INSERT INTO occupancy_type (code) VALUES (\'BUSY\');
+INSERT INTO occupancy_type (code) VALUES (\'OCCUPIED\');
+INSERT INTO shutter (point) VALUES (\'1.1.1.1\');
+INSERT INTO shutter (point) VALUES (\'street-red-building-33\');
+INSERT INTO shutter (point) VALUES (\'some-where\');
+INSERT INTO renting (shutter_id) SELECT id FROM shutter LIMIT 1;
+INSERT INTO renting (shutter_id) SELECT id FROM shutter OFFSET 2 LIMIT 1;
     ';
     const UNMOUNT = '
+DELETE FROM renting;
+DELETE FROM lease;
+DELETE FROM occupancy_type;
+DELETE FROM session;
+DELETE FROM unlock;
+DELETE FROM shutter;
+DELETE FROM "user";
+
 drop table IF EXISTS renting;
 drop table IF EXISTS lease;
 drop table IF EXISTS occupancy_type;
 drop table IF EXISTS session;
 drop table IF EXISTS unlock;
 drop table IF EXISTS shutter;
-drop table IF EXISTS user_role;
-drop table IF EXISTS role;
 drop table IF EXISTS "user";
 
 drop index IF EXISTS renting_shutter_id_uindex;
 drop index IF EXISTS occupancy_type_code_uindex;
-drop index IF EXISTS role_code_uindex;
 drop index IF EXISTS shutter_point_id_index;
 drop index IF EXISTS shutter_point_uindex;
 drop index IF EXISTS unlock_shutter_id_uindex;
@@ -156,7 +163,6 @@ drop index IF EXISTS user_email_secret_index;
 drop index IF EXISTS user_email_uindex;
 drop index IF EXISTS occupancy_user_id_start_finish_shutter_id_index;
 drop index IF EXISTS session_finish_token_user_id_index;
-drop index IF EXISTS user_role_user_id_role_id_uindex;
 
 VACUUM;
 ';
